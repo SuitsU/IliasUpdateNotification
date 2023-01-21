f99d3c92e7e3fe8c8392b1417398e64381b17cb5
chrisIlias1993 <christian.pietras@suitsu.de>
2023-01-21 09:10:33 +0000

    Version 1.0

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                 =   'updntf';
-$version            =   '0.1';
+$version            =   '1.0';
 $ilias_min_version  =   '7.0';
 $ilias_max_version  =   '7.9999';
 $responsible        =   'SUITS U GmbH';
